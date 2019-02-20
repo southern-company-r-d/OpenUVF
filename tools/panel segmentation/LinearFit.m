ce4db168f573956acae3d41654c494eff0691e41
Braden Gilleland <braden@gatech.edu>
2019-02-20 13:23:03 -0500

    Copyright Boilerplates added

--- tools/panel segmentation/LinearFit.m
+++ tools/panel segmentation/LinearFit.m
@@ -1,7 +1,20 @@
+% Copyright © 2019 Southern Company Services, Inc.  All rights reserved.
+%
+% Licensed under the Apache License, Version 2.0 (the "License");
+% you may not use this file except in compliance with the License.
+% You may obtain a copy of the License at
+%
+%     http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS,
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+% See the License for the specific language governing permissions and
+% limitations under the License.
+
 function [fit_rows, fit_columns, coefficients] = LinearFit(rows, cols, iterations)
 % LinearFit.m 
 % Joey Richardson - Southern Company Services 2018
-% Copyright © 2019 Southern Company Services, Inc.
 % Linear regression function for images. Used in the CrackIdentification
 % script when segmenting panels to find the best fit edges of each panel
 % side. 
